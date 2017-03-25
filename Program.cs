19fe9220ab16c7c1db2b5c376ccdcf22abad308d
thara0402 <thara0402@gmail.com>
2017-03-25 21:02:14 +0900

    add dockerfile

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace mvcapp
 {
@@ -11,11 +12,18 @@ namespace mvcapp
     {
         public static void Main(string[] args)
         {
+            // 環境変数からURLを取得する
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables("")
+                .Build();
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:5000";
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls(url)
                 .Build();
 
             host.Run();
